b33a4513a94ed897cdc902c82c9e13d94be2b760
stevenngo3 <s1ngo@ucsd.edu>
2022-05-11 23:36:34 -0700

    add test file

--- SkillDemo55.java
+++ SkillDemo55.java
@@ -1,6 +1,9 @@
 public class SkillDemo55{
 
 // hi
+    public static int subtract(int n, int m){
+        return n-m;
+    }
 
 
 }
---
+++ SkillDemo55Test.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemo55Test {
+    @Test
+    public void subtraction() {
+        assertEquals(2, SkillDemo55.sub(3,2));
+    }
+
+}
